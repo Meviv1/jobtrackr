8d337873b1e37b05ba459a75822049d9eb9cc4a4
Your Name <you@example.com>
2025-07-24 01:39:37 +0530

    Fix CORS for credentials

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,35 @@
-import express from "express"
-import mongoose from "mongoose"
-import dotenv from "dotenv"
-import cors from "cors"
-import authRoutes from "./routes/authRoutes.js"
-import jobRoutes from './routes/jobRoutes.js'
+// import express from "express"
+// import mongoose from "mongoose"
+// import dotenv from "dotenv"
+// import cors from "cors"
+// import authRoutes from "./routes/authRoutes.js"
+// import jobRoutes from './routes/jobRoutes.js'
 
-dotenv.config();
+// dotenv.config();
 
-const app = express();
-app.use(cors());
-app.use(express.json());
+// const app = express();
+// app.use(cors());
+// app.use(express.json());
 
-app.use("/api/auth",authRoutes);
-app.use("/api/job",jobRoutes);
+// app.use("/api/auth",authRoutes);
+// app.use("/api/job",jobRoutes);
 
-const PORT = process.env.PORT || 5000;
-mongoose.connect(process.env.MONGO_URI)
-.then(() => {
-    app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-})
-.catch((err) => console.log("MongoDB connection error:", err));
+// const PORT = process.env.PORT || 5000;
+// mongoose.connect(process.env.MONGO_URI)
+// .then(() => {
+//     app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+// })
+// .catch((err) => console.log("MongoDB connection error:", err));
+
+
+import cors from "cors";
+
+const allowedOrigins = [
+  "http://localhost:5173",                  // local dev
+  "https://jobtracko.vercel.app/"   
+];
+
+app.use(cors({
+  origin: allowedOrigins,
+  credentials: true,
+}));
